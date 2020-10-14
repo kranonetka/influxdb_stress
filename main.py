6ca519d4272847209933cff92258ee28759a6fa8
kranonetka <fakemail4steam@ya.ru>
2020-10-14 21:33:04 +0700

    Вынос логики в отдельную функцию

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from pathlib import Path
 
 from menu import get_menu
 
-if __name__ == '__main__':
+
+def main():
     with (Path(__file__).parent / 'influxdb_config.json').open(mode='r') as fp:
         config = json.load(fp)
     menu = get_menu(config)
     menu.call()
+
+
+if __name__ == '__main__':
+    main()
